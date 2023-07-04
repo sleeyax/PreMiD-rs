e0294dcb7a5314f60c46a34a79fc33dd1f852263
Sleeyax <yourd3veloper@gmail.com>
2023-07-04 22:24:50 +0200

    refactor: use hashmap for faster lookup speeds

--- src/rpc_client_manager.rs
+++ src/rpc_client_manager.rs
@@ -1,55 +1,41 @@
+use std::collections::HashMap;
+
 use crate::rpc_client::RpcClient;
 
 pub struct RpcClientManager {
-    rpc_clients: Vec<RpcClient>,
+    rpc_clients: HashMap<u64, RpcClient>,
 }
 
 impl RpcClientManager {
     pub fn new() -> Self {
         Self {
-            rpc_clients: Vec::new(),
+            rpc_clients: HashMap::new(),
         }
     }
 
-    pub fn add_client_instance(&mut self, rpc_client: RpcClient) {
+    pub fn add_client(&mut self, rpc_client: RpcClient) {
         println!(
             "[RPC Client Manager] add client instance {} ({} total instances registered)",
             &rpc_client.client_id,
             self.rpc_clients.len() + 1
         );
-        self.rpc_clients.push(rpc_client);
-    }
-
-    pub fn add_client(&mut self, client_id: u64) {
-        self.add_client_instance(RpcClient::new(client_id));
-    }
-
-    pub fn remove_client(&mut self, client_id: u64) {
-        let index = self
-            .rpc_clients
-            .iter()
-            .position(|client| client.client_id == client_id)
-            .unwrap();
-        self.rpc_clients.remove(index);
-    }
-
-    pub fn get_client(&self, client_id: u64) -> Option<&RpcClient> {
-        self.rpc_clients
-            .iter()
-            .find(|client| client.client_id == client_id)
+        self.rpc_clients.insert(rpc_client.client_id, rpc_client);
     }
 
     pub fn get_client_mut(&mut self, client_id: u64) -> Option<&mut RpcClient> {
-        self.rpc_clients
-            .iter_mut()
-            .find(|client| client.client_id == client_id)
+        self.rpc_clients.get_mut(&client_id)
     }
 
-    pub fn get_clients(&self) -> &Vec<RpcClient> {
-        &self.rpc_clients
-    }
+    pub fn clear_all_activities(&mut self) {
+        let keys: Vec<_> = self.rpc_clients.keys().cloned().collect();
 
-    pub fn get_clients_mut(&mut self) -> &mut Vec<RpcClient> {
-        &mut self.rpc_clients
+        for key in keys {
+            match self.rpc_clients.entry(key) {
+                std::collections::hash_map::Entry::Occupied(mut entry) => {
+                    entry.get_mut().clear_activity();
+                }
+                _ => {}
+            }
+        }
     }
 }
